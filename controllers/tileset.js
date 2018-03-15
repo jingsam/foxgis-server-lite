a39c87c3adf7c1efc44a9bf9f18ab54257b9bc1e
jingsam <jing-sam@qq.com>
2018-03-15 10:37:07 +0800

    change default port as 1234

--- controllers/tileset.js
+++ controllers/tileset.js
@@ -2,20 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const MBTiles = require('@mapbox/mbtiles')
 
-
 module.exports.list = (req, res, next) => {
   const tilesetsDir = path.resolve('./data/tilesets')
 
   fs.readdir(tilesetsDir, (err, files) => {
     if (err) return next(err)
 
-    res.json(files
-      .filter(file => path.extname(file) === '.mbtiles')
-      .map(file => path.parse(file).name)
+    res.json(
+      files
+        .filter(file => path.extname(file) === '.mbtiles')
+        .map(file => path.parse(file).name)
     )
   })
 }
-
 
 module.exports.getTileJSON = (req, res, next) => {
   const { tilesetId } = req.params
@@ -30,14 +29,15 @@ module.exports.getTileJSON = (req, res, next) => {
       if (err) return next(err)
 
       const endpoint = `${req.protocol}://${req.headers.host}/api/v1`
-      info.tiles = [`${endpoint}/tilesets/${tilesetId}/{z}/{x}/{y}.${info.format}`]
+      info.tiles = [
+        `${endpoint}/tilesets/${tilesetId}/{z}/{x}/{y}.${info.format}`
+      ]
       info.scheme = 'xyz'
 
       res.json(info)
     })
   })
 }
-
 
 module.exports.getTile = (req, res, next) => {
   const { tilesetId, z, x, y } = req.params
@@ -49,6 +49,8 @@ module.exports.getTile = (req, res, next) => {
     source.getTile(+z, +x, +y, (err, data, headers) => {
       source.close()
 
+      if (err && err.message.match(/(Tile|Grid) does not exist/))
+        return res.sendStatus(404)
       if (err) return next(err)
       if (!data) return res.sendStatus(404)
 
