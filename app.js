245eb85317752e925d37ceccbfd17d281b7da416
jingsam <jing-sam@qq.com>
2018-04-12 11:59:30 +0800

    refactor

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const compression = require('compression')
 const helmet = require('helmet')
 const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const cors = require('cors')
+const createError = require('http-errors')
 const router = require('./router')
 
 const app = express()
@@ -13,21 +12,19 @@ const app = express()
 app.set('json spaces', 2)
 app.set('trust proxy', true)
 
-app.use(compression())
+app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common'))
 app.use(helmet())
 app.use(cors())
-app.use(morgan('dev'))
-app.use(bodyParser.json({ limit: '5mb' }))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride())
-app.use(cookieParser())
+app.use(compression())
+app.use(express.json({ limit: '5mb' }))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.use('/api/v1', router)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.sendStatus(404)
+  next(createError(404))
 })
 
 // error handler
@@ -36,8 +33,10 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.json({
     message: err.message,
-    error: app.get('env') === 'production' ?
-      undefined : err.stack && err.stack.split('\n')
+    error:
+      process.env.NODE_ENV === 'development'
+        ? err.stack && err.stack.split('\n')
+        : []
   })
 })
 
